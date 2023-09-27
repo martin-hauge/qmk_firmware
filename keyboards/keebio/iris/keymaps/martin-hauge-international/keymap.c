1a64519b1bccba346313d86ed4d84fe90f5b4968
Martin Hauge <martinhauge@hotmail.com>
2023-09-28 00:03:39 +0200

    undeaden some keys, add semicolon

--- keyboards/keebio/iris/keymaps/martin-hauge-international/keymap.c
+++ keyboards/keebio/iris/keymaps/martin-hauge-international/keymap.c
@@ -12,7 +12,12 @@ enum custom_keycodes {
   QWERTY = SAFE_RANGE,
   LOWER,
   RAISE,
-  ADJUST
+  ADJUST,
+  M_GRAVE,
+  M_BSL,
+  M_TILDE,
+  M_SQUOT,
+  M_DQUOT
 };
 
 const key_override_t arrow_key_u_shift_override = ko_make_basic(MOD_MASK_SHIFT, KC_UP, KC_PGUP);
@@ -34,9 +39,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    ALGR(KC_W),
+     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    C(A(KC_W)),
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, KC_A,    KC_S,    LT(_LOWER, KC_D), SFT_T(KC_F), KC_G,                   KC_H, RSFT_T(KC_J), LT(_RAISE, KC_K), KC_L, ALGR(KC_L), ALGR(KC_Z),
+     KC_LCTL, KC_A,    KC_S,    LT(_LOWER, KC_D), SFT_T(KC_F), KC_G,                   KC_H, RSFT_T(KC_J), LT(_RAISE, KC_K), KC_L, C(A(KC_L)), C(A(KC_Z)),
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_MUTE,          KC_MPLY, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
@@ -49,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_TILD, KC_EXLM, UC(0x40), UC(0xA3), UC(0x24), KC_PERC,                         KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     PTAB,    _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, SEND_STRING("~ "),
+     PTAB,    _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, M_TILDE,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      _______, _______, _______, _______, _______, _______,                            _______, _______, KC_UP,   _______, _______,  KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
@@ -62,13 +67,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_RAISE] = LAYOUT(
 		  // SYMBOLS                                                            NUMPAD
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     SEND_STRING("` "), S(KC_1), SEND_STRING("\" "), SAGR(KC_4), S(KC_4), S(KC_5),    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
+     M_GRAVE, S(KC_1), M_BSL, SAGR(KC_4), S(KC_4), S(KC_5),    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      NTAB,    S(KC_EQL), KC_HASH, KC_LBRC, KC_RBRC, SAGR(KC_QUOT),                    _______, KC_P7,   KC_P8,   KC_P9,   KC_PMNS, KC_PAST,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, S(KC_QUOT), KC_QUOT, KC_BSLS, KC_SLSH, S(KC_SLSH),                      KC_EQL,  KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_PDOT,
+     _______, M_DQUOT, M_SQUOT, KC_BSLS, KC_SLSH, S(KC_SLSH),                      KC_EQL,  KC_P4,   KC_P5,   KC_P6,   KC_PPLS, KC_PDOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, S(KC_COMM), S(KC_DOT), S(KC_LBRC), S(KC_RBRC), KC_MINS, KC_LPRN, _______, _______, KC_P1, KC_P2,   KC_P3,   KC_PENT, _______,
+     _______, S(KC_COMM), S(KC_DOT), S(KC_LBRC), S(KC_RBRC), KC_SCLN, KC_LPRN, _______, _______, KC_P1, KC_P2,   KC_P3,   KC_PENT, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, _______, _______,                   _______, _______, KC_P0
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -125,6 +130,36 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       }
       return false;
       break;
+    case M_GRAVE:
+        if (record->event.pressed) {
+            SEND_STRING("` ");
+        } else {
+        }
+        break;
+    case M_BSL:
+        if (record->event.pressed) {
+            SEND_STRING("\" ");
+        } else {
+        }
+        break;
+    case M_TILDE:
+        if (record->event.pressed) {
+            SEND_STRING("~ ");
+        } else {
+        }
+        break;
+    case M_DQUOT:
+        if (record->event.pressed) {
+            SEND_STRING(S("' "));
+        } else {
+        }
+        break;
+    case M_SQUOT:
+        if (record->event.pressed) {
+            SEND_STRING("' ");
+        } else {
+        }
+        break;
   }
   return true;
 }
